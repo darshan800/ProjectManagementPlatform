0dde155514183bb5b6c720ae576ba5c6ec2b5de0
Darshan K <darshankulkarni34@gmail.com>
2026-03-19 16:28:19 +0530

    Controllers for task models

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,57 +1,85 @@
 import { User } from "../models/user.models.js";
 import { Project } from "../models/project.models.js";
 import { SubTask } from "../models/subtask.models.js";
-import { Tasks } from "../models/task.models.js";
+import { Task } from "../models/task.models.js";
 import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import mongoose from "mongoose";
 import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
 
+const getTasks = asyncHandler(async (req, res) => {
+  const { projectId } = req.params;
 
-const getTasks=asyncHandler(async(req,res)=>{
+  const tasks = await Task.find({ project: projectId }).populate(
+    "assignedTo",
+    "avatar username fullname",
+  );
 
-})
+  if (!tasks) {
+    throw new ApiError(404, "Task does not found ");
+  }
 
-const createTask=asyncHandler(async(req,res)=>{
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tasks, "task fetched succesfully"));
+});
 
-})
+const createTask = asyncHandler(async (req, res) => {
+  const { title, description, status, assignedTo } = req.body;
+  const { projectId } = req.params;
 
+  const project = await Project.findById(projectId);
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
 
-const getTaskById=asyncHandler(async(req,res)=>{
+  const files = req.files || [];
 
-})
+  const attachments = files.map((file) => {
+    return {
+      url: `${process.env.SERVER_URL}/images/${file.originalname}`,
+      mimetype: file.mimetype,
+      size: file.size,
+    };
+  });
 
+  const task = await Task.create({
+    title,
+    description,
+    project: new mongoose.Types.ObjectId(projectId),
+    assignedTo: assignedTo
+      ? new mongoose.Types.ObjectId(assignedTo)
+      : undefined,
+    status,
+    assignedBy: new mongoose.Types.ObjectId(req.user._id),
+    attachments,
+  });
 
-const updateTask=asyncHandler(async(req,res)=>{
+  return res
+    .status(201)
+    .json(new ApiResponse(201, task, "Task created succesfully"));
+});
 
-})
+const getTaskById = asyncHandler(async (req, res) => {});
 
+const updateTask = asyncHandler(async (req, res) => {});
 
-const deleteTask=asyncHandler(async(req,res)=>{
+const deleteTask = asyncHandler(async (req, res) => {});
 
-})
+const createSubTask = asyncHandler(async (req, res) => {});
 
+const updateSubTask = asyncHandler(async (req, res) => {});
 
-const createSubTask=asyncHandler(async(req,res)=>{
-
-})
-
-const updateSubTask=asyncHandler(async(req,res)=>{
-
-})
-
-const deleteSubTask=asyncHandler(async(req,res)=>{
-
-})
+const deleteSubTask = asyncHandler(async (req, res) => {});
 
 export {
-    createSubTask,
-    createTask,
-    updateSubTask,
-    updateTask,
-    deleteSubTask,
-    deleteTask,
-    getTasks,
-    getTaskById
-}
+  createSubTask,
+  createTask,
+  updateSubTask,
+  updateTask,
+  deleteSubTask,
+  deleteTask,
+  getTasks,
+  getTaskById,
+};
--- src/models/task.models.js
+++ src/models/task.models.js
@@ -47,4 +47,4 @@ const taskSchema = new Schema(
   { timestamps: true },
 );
 
-export const Tasks = mongoose.model("Task", taskSchema);
+export const Task = mongoose.model("Task", taskSchema);
